58626214a5b405a3c76308cbc2a36f20553ccc58
cuebin <116683603+cuebin@users.noreply.github.com>
2023-04-04 22:30:04 +0200

    Vigenère cipher

---
+++ main.py
@@ -0,0 +1,35 @@
+
+def run():
+    tekst = input("Streng: ").replace(' ', '')
+    nøkkel = input("Nøkkel: ").replace(' ', '')
+    kryptertTekst = vigenere_cipher(tekst, nøkkel)
+    print(kryptertTekst)
+
+def finnePosisjon(bokstav):    
+    alfabet = "abcdefghijklmnopqrstuvwxyz" #bruker ascii library til hente alfabetet i lowercase
+    plassering = alfabet.index(bokstav.lower()) #finner laveste index
+    return plassering
+
+def flyttePosisjon(bokstav, flytting): 
+    alfabet = "abcdefghijklmnopqrstuvwxyz"
+    plassering = (finnePosisjon(bokstav) + flytting) % 26 
+    nyBokstav = alfabet[plassering]
+    return nyBokstav
+
+def vigenere_cipher(tekst, nøkkel):
+    kryptertTekst = ""
+    counter = 0
+    for bokstav in tekst:
+        if bokstav in "abcdefghijklmnopqrstuvwxyz":
+            flytting = finnePosisjon(nøkkel[counter % len(nøkkel)])
+            nyBokstav = flyttePosisjon(bokstav, flytting)
+            kryptertTekst +=nyBokstav 
+            counter += 1
+        else:
+            kryptertTekst +=bokstav
+            
+    return kryptertTekst
+
+
+if __name__ == '__main__':
+    run()
